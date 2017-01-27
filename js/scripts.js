026ce550b997023c984ba29cdf321c03f746cd22
Jiwon Kang <jiwonk42@gmail.com>
2017-01-27 09:46:46 -0800

    create a Pizza.prototype.totalCost that loops through toppings array and add each topping cost to total cost

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,32 @@
 function Pizza(toppings, size) {
   this.toppings = toppings;
   this.pizzaSize = size;
+  this.totalCost = 0;
+}
+
+// Total Cost Prototype
+Pizza.prototype.totalCost = function() {
+
+  var totalCost = 0;
+
+  this.toppings.forEach(function(topping) {
+    if (topping === "Cheese") {
+      this.totalCost = 3;
+    } else if (topping === "Pepperoni") {
+      this.totalCost = 1;
+    } else if (topping === "Mushrooms") {
+      this.totalCost = 1;
+    } else if (topping === "Sausage") {
+      this.totalCost = 3;
+    } else if (topping === "Bell Pepper") {
+      this.totalCost = 2;
+    } else { // Onions
+      this.totalCost = 1;
+    }
+    totalCost += this.totalCost;
+  });
+  
+  this.totalCost = totalCost;
 }
 
 // User Interface Logic
@@ -16,9 +42,9 @@ $(document).ready(function() {
     $("input:checkbox[name=add-toppings]:checked").each(function(){
       inputtedToppings.push($(this).val());
     });
-
+    console.log(inputtedToppings);
     var inputtedSize = $("input:radio[name=select-a-size]:checked").val();
-
+    console.log(inputtedSize);
     var pizza = new Pizza(inputtedToppings, inputtedSize);
 
     $("order-detail h2").text(pizza);
