24f4e2edbd9df9dd47c3fdfe9622d9ad070051bf
Jiwon Kang <jiwonk42@gmail.com>
2017-01-27 09:35:23 -0800

    create two variables for toppings property pizza size property and a pizza object

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 // Business Logic
+
+// Pizza Constructor
 function Pizza(toppings, size) {
   this.toppings = toppings;
   this.pizzaSize = size;
@@ -8,5 +10,17 @@ function Pizza(toppings, size) {
 $(document).ready(function() {
   $("form#order-pizza").submit(function(event) {
     event.preventDefault();
+
+    var inputtedToppings = []; // Array for Toppings
+
+    $("input:checkbox[name=add-toppings]:checked").each(function(){
+      inputtedToppings.push($(this).val());
+    });
+
+    var inputtedSize = $("input:radio[name=select-a-size]:checked").val();
+
+    var pizza = new Pizza(inputtedToppings, inputtedSize);
+
+    $("order-detail h2").text(pizza);
   });
 });
